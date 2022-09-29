0f87df14025e79e609329a71b617b069be344659
Eduardo <eddy_wallace@hotmail.com>
2022-09-29 15:23:26 -0500

    - Corrección de Header en CheckToken

--- src/main/java/com/bmore/hyperius/web/rest/LoginJWTRest.java
+++ src/main/java/com/bmore/hyperius/web/rest/LoginJWTRest.java
@@ -46,7 +46,7 @@ public class LoginJWTRest {
   }
 
   @PostMapping(value = "/login/check-token", produces = MediaType.APPLICATION_JSON_VALUE)
-  public JwtLoginResponse checkToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+  public JwtLoginResponse checkToken(@RequestHeader("Auth") String token) {
     log.info("CheckTokenTest: " + token);
     return loginJwtService.updateJwt(token);
   }
