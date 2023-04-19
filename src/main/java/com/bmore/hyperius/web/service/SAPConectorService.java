9fe1a58e95635d80f2fb6b3d330784461b781aa0
Alfredo Sierra Mendoza <alfredo.mendoza@b-more.tech>
2023-04-19 10:00:34 -0600

    Revert "jco lib"
    This reverts commit f635c5fcb3e6b96b294f07014025825a6f3b11af.

--- src/main/java/com/bmore/hyperius/web/service/SAPConectorService.java
+++
@@ -1,9 +0,0 @@
-package com.bmore.hyperius.web.service;
-
-import com.sap.conn.jco.JCoDestination;
-
-public interface SAPConectorService {
-
-	public void inventorySnapShot(JCoDestination destination) ;
-
-}
--- src/main/java/com/bmore/hyperius/web/service/impl/CifrasControlServiceImpl.java
+++ src/main/java/com/bmore/hyperius/web/service/impl/CifrasControlServiceImpl.java
@@ -25,9 +25,6 @@ import com.bmore.hyperius.web.dto.ReporteAvanceDTO;
 import com.bmore.hyperius.web.dto.ResultDTO;
 import com.bmore.hyperius.web.repository.CifrasControlRepository;
 import com.bmore.hyperius.web.service.CifrasControlService;
-import com.bmore.hyperius.web.utils.ConnectionManager;
-import com.bmore.hyperius.web.utils.SAPConector;
-import com.sap.conn.jco.JCoDestination;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -40,19 +37,6 @@ public class CifrasControlServiceImpl implements CifrasControlService {
 
   @Override
   public DescargaInformacionDTO getDescargaInformacion() {
-
-    ConnectionManager connectionManager = new ConnectionManager();
-    // Quitar este 
-    JCoDestination destination;
-    try {
-      destination = connectionManager.getSapConnection("ECC PRD");
-      new SAPConector(destination).start();// , con , docInvBean
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-			
-      
     DescargaInformacionDTO descargaInformacionDTO = new DescargaInformacionDTO();
     // CifrasControlRepositoryOld cifrasControlDAO = new
     // CifrasControlRepositoryOld();
